a192e144ccc6439cf189ddb43fb2646b98905bc3
caputdraconis050630 <caputdraconis@kakao.com>
2021-08-18 16:20:32 +0900

    Flash Chatting App Prj

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flash_chat_flutter/screens/welcome_screen.dart';
+import 'package:flash_chat_flutter/screens/login_screen.dart';
+import 'package:flash_chat_flutter/screens/registration_screen.dart';
+import 'package:flash_chat_flutter/screens/chat_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id : (context) => WelcomeScreen(),
+        'login_screen' : (context) => LoginScreen(),
+        'chat_screen' : (context) => ChatScreen(),
+        'registration_screen' : (context) => RegistrationScreen(),
+      }
+    );
+  }
+}
