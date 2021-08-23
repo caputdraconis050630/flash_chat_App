1d801b9381cfaf5aaa42f8dc81895b883b69ec37
caputdraconis050630 <caputdraconis@kakao.com>
2021-08-23 11:18:46 +0900

    Add message sender(to firebase)

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flash_chat_flutter/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
 
@@ -13,16 +14,16 @@ class ChatScreen extends StatefulWidget {
 
 
 class _ChatScreenState extends State<ChatScreen> {
-
+  final _firestore = FirebaseFirestore.instance;
   final _auth = FirebaseAuth.instance;
-
   User? loggedInUser;
+  String? messageText;
+
   void getCurrentUser() async{
     try {
       final user = await _auth.currentUser!;
       if (user != null) {
         loggedInUser = user;
-        print(loggedInUser!.email);
       }
     }
     catch (e){
@@ -67,14 +68,18 @@ class _ChatScreenState extends State<ChatScreen> {
                   Expanded(
                     child: TextField(
                       onChanged: (value) {
-                        //Do something with the user input.
+                        messageText = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
-                  FlatButton(
+                  TextButton(
                     onPressed: () {
-                      //Implement send functionality.
+                      // messageText + loggedInUser.email
+                      _firestore.collection('messages').add({
+                        'sender': loggedInUser!.email,
+                        'text': messageText,
+                      });
                     },
                     child: Text(
                       'Send',
